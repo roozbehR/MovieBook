de4a9e39703ff5f18f676be18406440336824e6a
Bassel Ashi <passel.y@el-ashi.net>
2021-02-16 01:59:51 +0200

    Feature/data (#5)
    * Added models with hard-coded data
    
    * Added correct exports to be used from react components

---
+++ src/models/comment.js
@@ -0,0 +1,28 @@
+import { getRandomReview } from "./review";
+import { getRandomUser } from "./user";
+
+class Comment {
+    constructor(id, review, user, text) {
+        this.id = id;
+        this.review = review;
+        this.user = user;
+        this.text = text;
+    }
+}
+
+// Got movies from https://github.com/FEND16/movie-json-data/blob/master/json/movies-in-theaters.json
+let Comments = [
+    new Comment(1, getRandomReview(), getRandomUser(), 'Blah blah a b c okay blah!'),
+    new Comment(2, getRandomReview(), getRandomUser(), 'Totally agree!'),
+    new Comment(3, getRandomReview(), getRandomUser(), 'Lorem ipsum never ends, keep it up!'),
+    new Comment(4, getRandomReview(), getRandomUser(), 'The plot twist blew my mind, I totally agree!'),
+    new Comment(5, getRandomReview(), getRandomUser(), 'Hey, I am a big fan of this movie. I dont think its that bad!')
+];
+
+export function getRandomComment() {
+    return Comments[Math.floor(Math.random() * Comments.length)];
+}
+
+export function getComment(id) {
+    return Comments.filter(m => m.id == id);
+}
---
+++ src/models/user.js
@@ -0,0 +1,42 @@
+import { getMovie } from "./movie";
+
+class User {
+    constructor(id, username, password, fullName, picture, biography, isAdmin) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.picture = picture;
+        this.biography = biography;
+        this.isAdmin = isAdmin
+        this.likedMovies = [];
+        this.followingUsers = [];
+    }
+
+    likeMovie(movieId) {
+        const movie = getMovie(movieId);
+        this.likedMovies.push(movie);
+    }
+
+    followUser(userId) {
+        const user = getUser(userId);
+        this.followingUsers.push(user);
+    }
+}
+
+// Got movies from https://github.com/FEND16/movie-json-data/blob/master/json/movies-in-theaters.json
+let Users = [
+    new User(1, 'basselashi', 'test123', 'Bassel Ashi', 'https://randomuser.me/api/portraits/men/27.jpg', 'A movie enthusiast by nature, I love horror movies!', false),
+    new User(2, 'mamrkhan', 'test123', 'Mohammad Amr Khan', 'https://randomuser.me/api/portraits/men/28.jpg', 'Lorem ipsum whatever', true),
+    new User(3, 'roozbehy', 'test123', 'Roozbeh Yadollahi', 'https://randomuser.me/api/portraits/men/29.jpg', 'Another lorem ipsum about my bio', true),
+    new User(4, 'shisein', 'test123', 'Shisei Naka', 'https://randomuser.me/api/portraits/men/30.jpg', 'Welcome to my profile xyz', true),
+    new User(5, 'jdoe', 'test123', 'Jane Doe', 'https://randomuser.me/api/portraits/women/57.jpg', 'I dont know but sure why not', false),
+];
+
+export function getRandomUser() {
+    return Users[Math.floor(Math.random() * Users.length)];
+}
+
+export function getUser(id) {
+    return Users.filter(m => m.id == id);
+}
