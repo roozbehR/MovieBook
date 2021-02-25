b99ea8f0c65261bb6c535e5efdea6eceacd35fff
shiseru <shiseru@users.noreply.github.com>
2021-02-25 19:55:54 +0000

    apply auto-prettier

--- src/react-components/movie-gallery-page/movie-gallery-page.js
+++ src/react-components/movie-gallery-page/movie-gallery-page.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import BackgroundWrapper from '../background-wrapper/background-wrapper';
-import MovieGallery from '../movie-gallery/movie-gallery';
-import NavBar from '../navbar/navbar';
-import './style.css';
+import React from "react";
+import BackgroundWrapper from "../background-wrapper/background-wrapper";
+import MovieGallery from "../movie-gallery/movie-gallery";
+import NavBar from "../navbar/navbar";
+import "./style.css";
 
-export default function MovieGalleryPage(){
+export default function MovieGalleryPage() {
   return (
-      <BackgroundWrapper>
-        <NavBar />
+    <BackgroundWrapper>
+      <NavBar />
 
-        <div className="movieGalleryContainer">
-          <MovieGallery />
-        </div>
-      </BackgroundWrapper>
+      <div className="movieGalleryContainer">
+        <MovieGallery />
+      </div>
+    </BackgroundWrapper>
   );
-};
-
-
+}
--- src/react-components/movie-gallery/movie-gallery.js
+++ src/react-components/movie-gallery/movie-gallery.js
@@ -1,38 +1,61 @@
 import React from "react";
 import { Images } from "../themes";
-import "./style.css"
+import "./style.css";
 
-export default function MovieGallery(){
+export default function MovieGallery() {
   return (
-        <div className="contentsContainer">
-          <div>
-            <div className="titleContainer">
-              <div className="titleText">Recent Movies</div>
-            </div>
+    <div className="contentsContainer">
+      <div>
+        <div className="titleContainer">
+          <div className="titleText">Recent Movies</div>
+        </div>
 
-            <div className="imagesContainer">
-              <div className="row">
-                <div>
-                  <img className="imageStyle" src={Images.legoMovie} alt="legoMovie" />
-                </div>
-                <div>
-                  <img className="imageStyle" src={Images.blackPanther} alt="legoMovie" />
-                </div>
-                <img className="imageStyle" src={Images.greenlandMovie} alt="legoMovie" />
-              </div>
-
-              <div className="row">
-                <div>
-                  <img className="imageStyle" src={Images.greenlandMovie} alt="legoMovie" />
-                </div>
-                <div>
-                  <img className="imageStyle" src={Images.blackPanther} alt="legoMovie" />
-                </div>
-                <img className="imageStyle" src={Images.legoMovie} alt="legoMovie" />
-              </div>
+        <div className="imagesContainer">
+          <div className="row">
+            <div>
+              <img
+                className="imageStyle"
+                src={Images.legoMovie}
+                alt="legoMovie"
+              />
             </div>
+            <div>
+              <img
+                className="imageStyle"
+                src={Images.blackPanther}
+                alt="legoMovie"
+              />
+            </div>
+            <img
+              className="imageStyle"
+              src={Images.greenlandMovie}
+              alt="legoMovie"
+            />
+          </div>
+
+          <div className="row">
+            <div>
+              <img
+                className="imageStyle"
+                src={Images.greenlandMovie}
+                alt="legoMovie"
+              />
+            </div>
+            <div>
+              <img
+                className="imageStyle"
+                src={Images.blackPanther}
+                alt="legoMovie"
+              />
+            </div>
+            <img
+              className="imageStyle"
+              src={Images.legoMovie}
+              alt="legoMovie"
+            />
           </div>
         </div>
+      </div>
+    </div>
   );
-};
-
+}
