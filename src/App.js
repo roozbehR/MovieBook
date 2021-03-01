f7e200744024077b6dbd0456a51f52bab8878462
Mohammad Amr Khan <mohammedamrkhan@gmail.com>
2021-03-01 11:16:49 -0500

    Added boiler template for Admin Panel, and routing in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import ProfilePage from "./react-components/profile_page/ProfilePage";
 import HomePage from "./HomePage";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import MovieGalleryPage from "./react-components/movie-gallery-page/movie-gallery-page";
-import MoviePage from "./Movie"
+import MoviePage from "./Movie";
+import Admin from "./react-components/admin_panel/Admin"
 
 class App extends React.Component {
   render() {
@@ -18,12 +19,9 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" render={() => <HomePage />}></Route>
             <Route exact path="/profile" render={() => <ProfilePage />}></Route>
-            <Route
-              exact
-              path="/movies"
-              render={() => <MovieGalleryPage />}
-            ></Route>
+            <Route exact path="/movies" render={() => <MovieGalleryPage />}></Route>
             <Route exact path="/movie" render={() => <MoviePage />}></Route>
+            <Route exact path="/admin" render={() => <Admin />}></Route>
           </Switch>
         </BrowserRouter>
       </div>
---
+++ src/react-components/admin_panel/Admin.js
@@ -0,0 +1,28 @@
+import React from "react";
+import NavBar from "../navbar/navbar";
+import {
+  Layout,
+  Divider,
+  Tabs,
+  Descriptions,
+  Avatar,
+  Card,
+  Row,
+  Col,
+  Button,
+  Comment,
+} from "antd";
+import "./style.css";
+
+
+class Admin extends React.Component {
+    render() {
+      return (
+        <div className="AdminPage">
+          Hello
+        </div>
+      );
+    }
+  }
+  
+  export default Admin;
