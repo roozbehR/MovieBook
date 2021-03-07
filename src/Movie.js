7d5bf4bca11153e58437fd6da4d064ea218d4675
shiseru <nathaniel.naka@gmail.com>
2021-03-07 22:48:16 +0900

    added Navbar

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Movie from "./react-components/movie/movie";
+import NavBar from './react-components/navbar/navbar';
 
 import "./movie.css";
 
 class MoviePage extends React.Component {
   render() {
     return (
-      <div className="movie-container">
-        <Movie />
+      <div>
+        <NavBar />
+        <div className="movie-container">
+          <Movie />
+        </div>
       </div>
     );
   }
