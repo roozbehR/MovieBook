4c45ee0354023c2338966ed2e81f6746bd563097
shiseru <nathaniel.naka@gmail.com>
2021-03-07 22:44:38 +0900

    update navbar styling based on loocalstorage

--- src/react-components/navbar/navbar.js
+++ src/react-components/navbar/navbar.js
@@ -1,19 +1,30 @@
-import React from "react";
-import SearchBar from "./search-bar";
-import {MenuTab} from './menu-tab';
+import React, {useState, useEffect} from "react";
+import {NotAuthenticatedNavBar} from './not-authenticated-navbar';
+import AuthenticatedNavBar from './authenticated-navbar';
 import "./styles.css";
+import {getRandomUser} from '../../models/user';
 
 export default function NavBar() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // remove it later. test purpose
+    setUser(getRandomUser());
+
+    const json = localStorage.getItem("user");
+    const savedUser = JSON.parse(json);
+    if (savedUser){
+      setUser(savedUser);
+    }
+  }, []);
+
   return (
     <div>
-      <div className="flex-box">
-        <div className="header-menu">
-          <MenuTab path='/'>Home</MenuTab>
-          <MenuTab path='/movies'>Movie</MenuTab>
-          <MenuTab path='/movie'>Review</MenuTab>
-        </div>
-        <SearchBar />
-      </div>
+      { user?
+          <AuthenticatedNavBar userName={user.fullName} profileImagePath={user.picture}/>
+        :
+          <NotAuthenticatedNavBar />
+      }
     </div>
   );
 }
