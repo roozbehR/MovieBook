f4fb2e9b2785877416fe52af967f52a442d1c553
Bassel Ashi <passel.y@el-ashi.net>
2021-03-30 15:45:51 +0200

    Implemented user routes and session handling (#54)

--- models/users.js
+++ models/users.js
@@ -1,21 +1,84 @@
-/* User Mongoose Model */
-const mongoose = require('mongoose')
+'use strict';
 
-const User = mongoose.model('Users', {
-  id: Number,
-  username: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String
-  },
-  fullName: String,
-  picture: String,
-  biography: String,
-  isAdmin: Boolean,
-  likedMovies: [],
-  followingUser: []
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    id: Number,
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+    password: {
+        type: String,
+        minlength: 6
+    },
+    fullName: String,
+    picture: String,
+    biography: String,
+    isAdmin: Boolean,
+    likedMovies: [],
+    followingUser: []
+});
+
+/********************************* User Middleware Below ************************************/
+// Hash password before saving it
+UserSchema.pre('save', function (next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        });
+    } else {
+        next();
+    }
 })
-module.exports = { User }
+
+/********************************* User Statics Below ************************************/
+// Find user given username and password
+UserSchema.statics.findByUsernamePassword = async function (username, password) {
+    const User = this;
+
+    const user = await User.findOne({ username: username });
+    if (!user)
+        return Promise.reject(false);
+
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, user.password, (err, result) => {
+            if (result) {
+                resolve(user);
+            } else {
+                reject();
+            }
+        });
+    })
+}
+
+// Find user given username
+UserSchema.statics.findByUsername = async function (username) {
+    const User = this;
+    const existingUser = User.findOne({ username: username });
+    return existingUser
+}
+
+// Create user given username and password
+UserSchema.statics.createUser = async function (user) {
+    const User = this;
+    try {
+        const existingUser = await User.findByUsername(user.username);
+        if (existingUser)
+            return Promise.resolve(false);
+        return user.save();
+    } catch (error) {
+        return Promise.reject(error);
+    }
+}
+
+const User = mongoose.model('User', UserSchema)
+module.exports = { User };
