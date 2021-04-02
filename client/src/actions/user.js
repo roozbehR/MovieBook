edd2d78ed5a7eb7a5b8a088cbb9a3ea862f04978
roozbehR <61944934+roozbehR@users.noreply.github.com>
2021-04-02 17:49:36 -0400

    Integrated the signup form from frontend to backed

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,3 +1,5 @@
+import { json } from "body-parser";
+
 const API_HOST = 'http://localhost:5000'
 
 // Check if user session exists
@@ -25,7 +27,7 @@ export const checkSession = (app) => {
         });
 };
 
-export const login = (loginComp, app) => {
+export const login = (loginComp) => {
     const request = new Request(`${API_HOST}/user/login`, {
         method: "post",
         body: JSON.stringify({
@@ -55,4 +57,33 @@ export const login = (loginComp, app) => {
         .catch(error => {
             console.log(error);
         });
+};
+
+export const signup = (signupComp) => {
+    console.log("Here in user.js");
+    const request = new Request(`${API_HOST}/user/register`, {
+        method: "post",
+        body: JSON.stringify({
+            username: signupComp.state.signup.username === '' ? 'newuser' : signupComp.state.signup.username,
+            password: signupComp.state.signup.password === '' ? 'correctPass' : signupComp.state.signup.password,
+            email: signupComp.state.signup.email === '' ? '309@mail.com' : signupComp.state.signup.email
+        }),
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+        },
+        credentials: 'include'
+    });
+    fetch(request)
+    .then(res => {
+        if (res.status === 200) {
+            return res.json();
+        }
+    })
+    .then(json => {
+        signupComp.handleLogin(json);
+    })
+    .catch(error => {
+        console.log(error);
+    });
 };
