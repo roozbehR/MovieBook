8e54b3524d1b9018d08937e08be9258a73c1c222
shiseru <nathaniel.naka@gmail.com>
2021-04-05 23:00:31 +0900

    fetch movies for movie gallery  (#63)
    * finished implementing movie-gallery
    
    * fixed image alt

--- client/src/actions/movies.js
+++ client/src/actions/movies.js
@@ -14,17 +14,17 @@ export const getRandomMovie = (rand) => {
         .then(json => {
             console.log(json);
             rand.setState({ randomReviews: json.review, randomMovie: json.movie[0]});
-            
+
             //return json.movie[0];
         })
         .catch(error => {
             console.log(error);
-        }); 
+        });
 };
 
 export const getTopMovies = (movies) => {
 	const url = `${API_HOST}/api/movie/top/movies`;
-	
+
 	fetch(url)
 		.then(res => {
 			if (res.status === 200) {
@@ -41,4 +41,26 @@ export const getTopMovies = (movies) => {
 	.catch(error => {
 		console.log(error);
 	});
+};
+
+export const getHookRandomMovie = () => {
+  const url = `${API_HOST}/api/movie/random/movie`
+
+  return fetch(url)
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          alert("error: could not get movie");
+        }
+      })
+      .then(json => {
+        console.log('hello I"m doing good')
+        console.log(json);
+        const return_object = { randomReviews: json.review, randomMovie: json.movie[0]}
+        return return_object;
+      })
+      .catch(error => {
+        console.log(error);
+      });
 };
