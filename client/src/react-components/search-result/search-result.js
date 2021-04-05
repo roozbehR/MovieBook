ed2144db8b8c1ce62b052e6b12d653169b59c373
shiseru <nathaniel.naka@gmail.com>
2021-04-06 00:40:43 +0900

    implemented Search

---
+++ client/src/react-components/search-result/search-result.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from "react";
+import { useParams } from 'react-router-dom'
+import {getSearchedMovies} from '../../actions/movies';
+import MovieOverview from '../movie-overview/movie-overview';
+import BackgroundWrapper from '../background-wrapper/background-wrapper';
+import NavBar from '../navbar/navbar';
+import {Spin} from 'antd';
+import styles from "./search-result.module.css";
+
+export default function SearchResult(){
+  const {input} = useParams();
+  const [movies, setMovies] = useState();
+
+  const fetchSearchedMovies = async () => {
+    const response = await getSearchedMovies(input);
+    await setMovies(response);
+  };
+
+  useEffect(() => {
+    fetchSearchedMovies().then(() => {
+    });
+  }, []);
+
+  return (
+      <BackgroundWrapper>
+        <NavBar />
+        <div className={styles.titleText}>Search Result</div>
+        {!movies && <div className={styles.loading}><Spin/></div>}
+        {movies &&
+        movies.slice(0, -1).map(movie => {
+          const passedProps  = {
+            sectionTitle: null,
+            imgURL: movie.poster,
+            movieTitle: movie.title,
+            screeningYear: movie.year,
+            plot: movie.plot
+          };
+          return <MovieOverview {...passedProps} />
+        })}
+      </BackgroundWrapper>
+  );
+}
